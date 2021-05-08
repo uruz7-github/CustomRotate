5980450c5e9589bce3aa045282ca389cde424654
liuyunfei2020 <yunfei.liu@centurygame.com>
2021-05-08 11:13:28 +0800

    add code

---
+++ Assets/MyRotate/Scripts/Editor/MyRotateEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+using CenturyGame.MyRotate;
+
+namespace CenturyGame.MyRotate.Editor
+{
+    public class MyRotateEditor
+    {
+        [MenuItem("MyRotate/CreateCube")]
+        public static void CreateCube()
+        {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            MyRotate myRotate = cube.AddComponent<MyRotate>();
+            cube.name = "NewCube";
+            myRotate.transform.position = new Vector3(0, 1, -5);
+            myRotate.transform.localEulerAngles = new Vector3(0, 60, 40);
+        }
+    }
+}
---
+++ Assets/MyRotate/Scripts/MyRotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CenturyGame.MyRotate
+{
+    public class MyRotate : MonoBehaviour
+    {
+        public float MyRotateSpeed = 10.0f;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            var rot = Quaternion.AngleAxis(MyRotateSpeed * Time.deltaTime, Vector3.up);
+            transform.localRotation = rot * transform.localRotation;
+        }
+    }
+}
